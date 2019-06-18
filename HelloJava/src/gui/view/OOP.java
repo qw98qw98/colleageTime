948fd1913559cf9271968de1beb4ced95371433a
Your Name <you@example.com>
2019-06-18 11:55:45 +0800

    gui java

--- HelloJava/src/gui/view/OOP.java
+++ HelloJava/src/gui/view/OOP.java
@@ -34,6 +34,10 @@ public class OOP extends javax.swing.JDialog {
         initComponents();
     }
 
+    OOP() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
